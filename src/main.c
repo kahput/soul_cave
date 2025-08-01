8b58b1443f7133ca2f387bcab4e6463ac5a48e3f
kahput <kahputs@gmail.com>
2025-08-01 16:07:48 +0200

    feat: create simple sandbox level

--- src/main.c
+++ src/main.c
@@ -51,15 +51,23 @@ int main(void) {
 	RenderTexture2D target = LoadRenderTexture(RESOLUTION_WIDTH, RESOLUTION_HEIGHT);
 	SetTargetFPS(60);
 
-	GameState state = {.level_arena = arena_alloc()};
+	GameState state = { .level_arena = arena_alloc() };
 	game_initialize(&state);
+
+	// Camera2D camera = { 0 };
+	// camera.target = (Vector2){ state.player.transform.position.x + TILE_SIZE * TILE_SCALE, state.player.transform.position.y + TILE_SIZE * TILE_SCALE };
+	// camera.offset = (Vector2){ RESOLUTION_WIDTH / 2.0f, RESOLUTION_HEIGHT / 2.0f };
+	// camera.rotation = 0.0f;
+	// camera.zoom = 1.0f;
 
 	while (!WindowShouldClose()) {
 		float dt = GetFrameTime();
 
 		game_update(&state, dt);
+		// camera.target = (Vector2){ state.player.transform.position.x + TILE_SIZE * TILE_SCALE, state.player.transform.position.y + TILE_SIZE * TILE_SCALE };
 
 		BeginTextureMode(target);
+		// BeginMode2D(camera);
 		ClearBackground(RAYWHITE);
 
 		renderer_begin_frame((void *)0);
@@ -75,28 +83,28 @@ int main(void) {
 		renderer_submit(&render_player);
 		renderer_end_frame();
 
+		// EndMode2D();
 		EndTextureMode();
 
 		BeginDrawing();
 		ClearBackground(BLACK);
 
-		float scale = fminf((float)SCREEN_WIDTH / RESOLUTION_WIDTH,
-			(float)SCREEN_HEIGHT / RESOLUTION_HEIGHT);
+		float scale = fminf((float)SCREEN_WIDTH / RESOLUTION_WIDTH, (float)SCREEN_HEIGHT / RESOLUTION_HEIGHT);
 
 		Rectangle source = {
-		  .x = 0.0f,
-		  .y = 0.0f,
-		  .width = RESOLUTION_WIDTH,
-		  .height = -RESOLUTION_HEIGHT,
+			.x = 0.0f,
+			.y = 0.0f,
+			.width = RESOLUTION_WIDTH,
+			.height = -RESOLUTION_HEIGHT,
 		};
 		Rectangle dest = {
-		  .x = (SCREEN_WIDTH - (RESOLUTION_WIDTH * scale)) / 2.f,
-		  .y = (SCREEN_HEIGHT - (RESOLUTION_HEIGHT * scale)) / 2.f,
-		  .width = RESOLUTION_WIDTH * scale,
-		  .height = RESOLUTION_HEIGHT * scale,
+			.x = (SCREEN_WIDTH - (RESOLUTION_WIDTH * scale)) / 2.f,
+			.y = (SCREEN_HEIGHT - (RESOLUTION_HEIGHT * scale)) / 2.f,
+			.width = RESOLUTION_WIDTH * scale,
+			.height = RESOLUTION_HEIGHT * scale,
 		};
 
-		DrawTexturePro(target.texture, source, dest, (Vector2){0}, 0.0f, WHITE);
+		DrawTexturePro(target.texture, source, dest, (Vector2){ 0 }, 0.0f, WHITE);
 		EndDrawing();
 	}
 
@@ -108,26 +116,27 @@ int main(void) {
 void game_initialize(GameState *state) {
 	Texture texture = LoadTexture("./assets/tiles/tilemap.png");
 	state->tile_sheet = (TileSheet){
-	  .texture = texture,
-	  .tile_size = TILE_SIZE,
-	  .gap = TILE_GAP,
-	  .columns = (texture.width + TILE_GAP) / (TILE_SIZE + TILE_GAP),
-	  .rows = (texture.height + TILE_GAP) / (TILE_SIZE + TILE_GAP),
+		.texture = texture,
+		.tile_size = TILE_SIZE,
+		.gap = TILE_GAP,
+		.columns = (texture.width + TILE_GAP) / (TILE_SIZE + TILE_GAP),
+		.rows = (texture.height + TILE_GAP) / (TILE_SIZE + TILE_GAP),
 	};
 
-	object_populate(&state->player, (Vector2){.x = TILE_SIZE, TILE_SIZE}, &state->tile_sheet, (IVector2){1, 7}, true);
+	object_populate(&state->player, (Vector2){ .x = TILE_SIZE, TILE_SIZE }, &state->tile_sheet, (IVector2){ 1, 7 }, true);
 	state->player.sprite.origin.y = state->player.sprite.src.height;
 	state->player.shape = (CollisionShape){
-	  .type = COLLISION_TYPE_RECTANGLE,
-	  .transform = {
-		.position = {
-		  .x = -state->player.sprite.src.width / 2.f * state->player.transform.scale.x,
-		  .y = -state->player.sprite.src.height / 2.f * state->player.transform.scale.y,
+		.type = COLLISION_TYPE_RECTANGLE,
+		.transform = {
+		  .position = {
+			.x = -state->player.sprite.src.width / 2.f * state->player.transform.scale.x,
+			.y = -state->player.sprite.src.height / 2.f * state->player.transform.scale.y,
+		  },
+		  .scale = { 1.f, 1.f },
 		},
-		.scale = {1.f, 1.f},
-	  },
-	  .width = TILE_SIZE,
-	  .height = TILE_SIZE / 2.f};
+		.width = TILE_SIZE,
+		.height = TILE_SIZE / 2.f
+	};
 
 	state->level = game_load_level(state->level_arena, "./assets/levels/level_01.txt", &state->tile_sheet);
 }
@@ -159,9 +168,9 @@ void game_update(GameState *state, float dt) {
 			// choose smaller overlap
 			Vector2 penetration;
 			if (min_overlap_x < min_overlap_y)
-				penetration = (overlap_left < overlap_right) ? (Vector2){overlap_left, 0} : (Vector2){-overlap_right, 0};
+				penetration = (overlap_left < overlap_right) ? (Vector2){ overlap_left, 0 } : (Vector2){ -overlap_right, 0 };
 			else
-				penetration = (overlap_up < overlap_down) ? (Vector2){0, overlap_up} : (Vector2){0, -overlap_down};
+				penetration = (overlap_up < overlap_down) ? (Vector2){ 0, overlap_up } : (Vector2){ 0, -overlap_down };
 
 			state->player.transform.position.x += penetration.x;
 			state->player.transform.position.y += penetration.y;
@@ -171,10 +180,10 @@ void game_update(GameState *state, float dt) {
 
 Rectangle get_collision_shape(Object *object) {
 	return (Rectangle){
-	  .x = object->transform.position.x + object->shape.transform.position.x,
-	  .y = object->transform.position.y + object->shape.transform.position.y,
-	  .width = object->shape.width * object->shape.transform.scale.x * object->transform.scale.x,
-	  .height = object->shape.height * object->shape.transform.scale.y * object->transform.scale.y,
+		.x = object->transform.position.x + object->shape.transform.position.x,
+		.y = object->transform.position.y + object->shape.transform.position.y,
+		.width = object->shape.width * object->shape.transform.scale.x * object->transform.scale.x,
+		.height = object->shape.height * object->shape.transform.scale.y * object->transform.scale.y,
 	};
 }
 
@@ -187,62 +196,62 @@ bool aabb_collision(Object *a, Object *b) {
 
 void object_populate(Object *object, Vector2 position, const TileSheet *tile_sheet, IVector2 texture_offset, bool centered) {
 	*object = (Object){
-	  .transform = {
-		.position = position,
-		.scale = {TILE_SCALE, TILE_SCALE},
-		.rotation = 0.0f,
-	  },
-	  .sprite = {
+		.transform = {
+		  .position = position,
+		  .scale = { TILE_SCALE, TILE_SCALE },
+		  .rotation = 0.0f,
+		},
+		.sprite = {
+		  .transform = {
+			.scale = {
+			  .x = 1.f,
+			  .y = 1.f,
+			},
+		  },
+		  .texture = tile_sheet->texture,
+		  .src = {
+			.x = (float)(tile_sheet->tile_size + tile_sheet->gap) * texture_offset.x,
+			.y = (float)(tile_sheet->tile_size + tile_sheet->gap) * texture_offset.y,
+			.width = tile_sheet->tile_size,
+			.height = tile_sheet->tile_size,
+		  },
+		  .origin = {
+			.x = 0.f,
+			.y = 0.f,
+		  },
+		},
+	};
+
+	object->shape = (CollisionShape){
+		.type = COLLISION_TYPE_RECTANGLE,
 		.transform = {
 		  .scale = {
 			.x = 1.f,
 			.y = 1.f,
 		  },
 		},
-		.texture = tile_sheet->texture,
-		.src = {
-		  .x = (float)(tile_sheet->tile_size + tile_sheet->gap) * texture_offset.x,
-		  .y = (float)(tile_sheet->tile_size + tile_sheet->gap) * texture_offset.y,
-		  .width = tile_sheet->tile_size,
-		  .height = tile_sheet->tile_size,
-		},
-		.origin = {
-		  .x = 0.f,
-		  .y = 0.f,
-		},
-	  },
-	};
-
-	object->shape = (CollisionShape){
-	  .type = COLLISION_TYPE_RECTANGLE,
-	  .transform = {
-		.scale = {
-		  .x = 1.f,
-		  .y = 1.f,
-		},
-	  },
-	  .width = object->sprite.src.width,
-	  .height = object->sprite.src.height,
+		.width = object->sprite.src.width,
+		.height = object->sprite.src.height,
 	};
 
 	if (centered) {
 		object->sprite.origin = (Vector2){
-		  .x = object->sprite.src.width / 2.f,
-		  .y = object->sprite.src.height / 2.f,
+			.x = object->sprite.src.width / 2.f,
+			.y = object->sprite.src.height / 2.f,
 		};
 		object->shape.transform.position = (Vector2){
-		  .x = -object->shape.width / 2.f * object->transform.scale.x,
-		  .y = -object->shape.height / 2.f * object->transform.scale.y,
+			.x = -object->shape.width / 2.f * object->transform.scale.x,
+			.y = -object->shape.height / 2.f * object->transform.scale.y,
 		};
 
 	} else {
 		object->sprite.origin = (Vector2){
-		  .x = 0.0f,
-		  .y = 0.0f,
+			.x = 0.0f,
+			.y = 0.0f,
 		};
 		object->shape.transform.position = (Vector2){
-		  .x = 0.0f,
-		  .y = 0.0f,
+			.x = 0.0f,
+			.y = 0.0f,
 		};
 	}
 }
@@ -294,14 +303,21 @@ Level *game_load_level(Arena *arena, const char *path, const TileSheet *tile_she
 				continue;
 			}
 
-			uint8_t x_off = texture_id % tile_sheet->columns;
-			uint8_t y_off = texture_id / tile_sheet->columns;
+			IVector2 texture_offset = {
+				.x = texture_id % tile_sheet->columns,
+				.y = texture_id / tile_sheet->columns,
+			};
 
 			Object *tile = level->objects + level->count++;
 
-			object_populate(tile, (Vector2){x * tile_sheet->tile_size * TILE_SCALE, y * tile_sheet->tile_size * TILE_SCALE}, tile_sheet, (IVector2){x_off, y_off}, false);
-			if (texture_id == 48)
+			if (texture_id == 48) {
+				bool variant = GetRandomValue(0, 10) == 0;
+				texture_offset.x += variant;
+
+				object_populate(tile, (Vector2){ x * tile_sheet->tile_size * TILE_SCALE, y * tile_sheet->tile_size * TILE_SCALE }, tile_sheet, texture_offset, false);
 				tile->shape.type = COLLISION_TYPE_NONE;
+			} else
+				object_populate(tile, (Vector2){ x * tile_sheet->tile_size * TILE_SCALE, y * tile_sheet->tile_size * TILE_SCALE }, tile_sheet, texture_offset, false);
 
 			token = strtok(NULL, " \t\r\n");
 		}
